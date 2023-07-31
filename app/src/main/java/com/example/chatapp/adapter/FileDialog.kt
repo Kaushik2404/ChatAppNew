8aaba3f10d09075e218aeb55c82ce34fa015e186
Jatin Lakhani <khilanvithano0080@gmail.com>
2023-07-31 17:52:39 +0530

    first commit

---
+++ app/src/main/java/com/example/chatapp/adapter/FileDialog.kt
@@ -0,0 +1,4 @@
+package com.example.chatapp.adapter
+
+class FileDialog {
+}
---
+++ app/src/main/java/com/example/chatapp/interfacefile/OnClickDilogFile.kt
@@ -0,0 +1,6 @@
+package com.example.chatapp.interfacefile
+
+interface OnClickGlaray {
+   fun onClickGalarySendImage()
+
+}
